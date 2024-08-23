7436431fbfbaa4b4fa85059a860dc2e7ebc0560f
obeidi <jobsobeidi@gmail.com>
2024-08-23 21:13:38 +0200

    add the springboot App

---
+++ src/main/java/com/example/db/spring_http_Interface/SpringHttpInterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.db.spring_http_Interface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHttpInterfaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHttpInterfaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/db/spring_http_Interface/SpringHttpInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.db.spring_http_Interface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHttpInterfaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
